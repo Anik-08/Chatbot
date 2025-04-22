177f1aef82cf34be25e6754cef8b89c5aac3ca9e
Anik-08 <anik.tiwary08@gmail.com>
2025-04-22 15:26:31 +0530

    Changes in env files

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pix3go&m@o1l-%(12-yn)r)zb##08&)8ghvomj@-=5l-r=)ray'
-
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-fallback-key-for-development')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
