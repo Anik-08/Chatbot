2c2ce33bf5f6ad6cc557875f7c3118c4349795bb
Anik-08 <anik.tiwary08@gmail.com>
2025-04-25 09:35:18 +0530

    deployment changes

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -124,6 +124,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "chatbot_app" / "static",
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
